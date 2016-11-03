730fdcf02c4cb569c045e99a864a00fc9faf8e66
dialmedu <diegomesa.1414@gmail.com>
2016-11-03 15:30:04 -0500

    Permite Traslacion en X y Y

--- js/marco.js
+++ js/marco.js
@@ -1,47 +1,93 @@
-var active_move = false;
-var ux = 0;
-var uy = 0;
+var movimientoActivado = false;
+var contenedorDelMapa = '';
+var ultimaPosicionEnX = 0;
+var traslacionParaEjeX = 0;
+var ultimaPosicionEnY = 0;
+var traslacionParaEjeY = 0;
 
-var umx = 0;
-var umy = 0;
 $(document).ready(function(){
-
-   	   $('.contenedor').mousemove(function(event){
-   	   		if (active_move) {
-	        	texto ="cX,cY: " + event.clientX + ", " + event.clientY + "\n";
-	        	texto +="pX,pY: " + event.pageX + ", " + event.pageY + "\n";
-	        	
-	        	var x = event.pageX - this.offsetLeft;
-	    		var y = event.pageY - this.offsetTop;
-	    		texto += "OffsL,OffsT: " + this.offsetLeft + ", " + this.offsetTop+ "\n";
-	        	texto += "CDX,CDY: " + x + ", " + y+ "\n";
-	        	texto += "UX:: " + ux;
-
-	        	$(".coordenadas").text(texto);
-	        	var mx = event.clientX-ux;
-	        	var my = event.clientY-uy;
-
-	        	if (mx<=0 && mx >= -1500) {
-	        		umx = mx;
-		        	
-	        	};
-	        	if (my<=0 && my >= -950) {
-		        	umy = my;
-	        	};
-
-	        	var translate3d = "transform: translate3d("+umx+"px,"+umy+"px,0px)";
-		        	console.log(translate3d);
-	   	   		$("img.fondo__imagen.imagen_mapa,img.frente__imagen.imagen_mapa")
-	   	   				.attr("style",translate3d);
-   	   		};
-    	});
-
-    	$('.contenedor').mouseup(function(event){
-    		active_move = !active_move;
-    		if (active_move) {
-    				ux = event.clientX;
-    				uy = event.clientY;	
-    		};
-    		
-    	});
+   	   mapa('.contenedor');
 });
+
+function mapa (objetoDelDom)
+{
+	contenedorDelMapa = objetoDelDom
+	$(contenedorDelMapa)
+		.mousedown(function(event)
+		{
+	    		movimientoActivado = true
+	    })
+		.mouseup(function(event)
+		{
+				movimientoActivado = false
+	    })
+		.mousemove(function(event)
+		{
+				if (movimientoActivado)
+				{
+					mover(event)
+				};
+		})
+}
+
+function mover(event) {
+	actualPosicionEnY = calcularActualPosicionEnY(event)
+	actualPosicionEnX = calcularActualPosicionEnX(event)
+	presicion = 10;
+
+	if (ultimaPosicionEnY > actualPosicionEnY)
+	{
+		traslacionParaEjeY +=presicion
+	}
+	else if(ultimaPosicionEnY < actualPosicionEnY)
+	{
+		traslacionParaEjeY -=presicion
+	}
+	if (ultimaPosicionEnX > actualPosicionEnX)
+	{
+		traslacionParaEjeX +=presicion
+	}
+	else if(ultimaPosicionEnX < actualPosicionEnX)
+	{
+		traslacionParaEjeX -=presicion
+	}
+
+
+	ultimaPosicionEnY = actualPosicionEnY
+	ultimaPosicionEnX = actualPosicionEnX
+
+	objetoDelDom = 'img.fondo__imagen.imagen_mapa'
+	objetoDelDom += ',img.frente__imagen.imagen_mapa'
+
+	trasladarObjeto(objetoDelDom,traslacionParaEjeX,traslacionParaEjeY,0);
+}
+
+function calcularActualPosicionEnY(event)
+{
+	actualPosicionEnY = (event.pageY - $(contenedorDelMapa).offset().top)
+	return actualPosicionEnY
+}
+
+function calcularActualPosicionEnX(event)
+{
+	actualPosicionEnX = (event.pageX - $(contenedorDelMapa).offset().left)
+	return actualPosicionEnX
+}
+
+function trasladarObjeto (objetoDelDom,valorX,valorY,valorZ) 
+{
+	$(objetoDelDom).attr('style',generarCssDeTraslacion(valorX,valorY))
+}
+ 
+function generarCssDeTraslacion(valorX,valorY,valorZ)
+{
+	if (valorX===undefined) { valorX=0 };
+	if (valorY===undefined) { valorY=0 };
+	if (valorZ===undefined) { valorZ=0 };
+
+	xPixeles = valorX + "px"
+	yPixeles = valorY + "px"
+	zPixeles = valorZ + "px"
+
+	return "transform: translate3d("+xPixeles+","+yPixeles+","+zPixeles+")"
+}
